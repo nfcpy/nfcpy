c18ebc00478a4f8b717b675201a8fc409ce1d3c2
stephen.tiedemann@googlemail.com <>
2011-07-04 20:05:12 +0200

    major rewrite of pn53x module to better leverage exception processing

--- nfc/dep.py
+++ nfc/dep.py
@@ -23,6 +23,8 @@
 import logging
 log = logging.getLogger(__name__)
 
+import time
+
 class DEP(object):
     def __init__(self, dev, general_bytes, role):
         self._dev = dev
@@ -39,10 +41,6 @@ class DEP(object):
         """Role in DEP communication, either 'Target' or 'Initiator'"""
         return self._role
 
-    @property
-    def maximum_transport_unit(self):
-        return self._dev.mtu
-
 class DEPInitiator(DEP):
     def __init__(self, dev, general_bytes):
         DEP.__init__(self, dev, general_bytes, "Initiator")
@@ -53,7 +51,13 @@ class DEPInitiator(DEP):
         returns a byte string with the response data, otherwise IOError
         exception is raised.
         """
-        return self._dev.dep_exchange(data, timeout)
+        log.debug("dep send {0} byte\n{1}".format(len(data), format_data(data)))
+        t0 = time.time()
+        data = self._dev.dep_exchange(data, timeout)
+        duration = int((time.time() - t0) * 1000)
+        log.debug("exchange() completed in {0} ms".format(duration))
+        log.debug("dep recv {0} byte\n{1}".format(len(data), format_data(data)))
+        return data
 
 class DEPTarget(DEP):
     def __init__(self, dev, general_bytes):
@@ -67,12 +71,31 @@ class DEPTarget(DEP):
         """Receive an NFCIP-1 DEP command. If a command is received within
         *timeout* milliseconds the data portion is returned as a byte 
         string, otherwise an IOError exception is raised."""
-        return self._dev.dep_get_data(timeout)
+        t0 = time.time()
+        data = self._dev.dep_get_data(timeout)
+        duration = int((time.time() - t0) * 1000)
+        log.debug("wait_command() completed in {0} ms".format(duration))
+        log.debug("dep recv {0} byte\n{1}".format(len(data), format_data(data)))
+        return data
 
-    def send_response(self, data, timeout, mtu=None):
+    def send_response(self, data, timeout):
         """Send an NFCIP-1 DEP response with the byte string *data*
         as the payload.
         """
+        log.debug("dep send {0} byte\n{1}".format(len(data), format_data(data)))
+        t0 = time.time()
         self._dev.dep_set_data(data, timeout)
+        duration = int((time.time() - t0) * 1000)
+        log.debug("send_response() completed in {0} ms".format(duration))
 
+def format_data(data):
+    import string
+    printable = string.digits + string.letters + string.punctuation + ' '
+    s = []
+    for i in range(0, len(data), 16):
+        s.append("  {offset:04x}: ".format(offset=i))
+        s[-1] += ' '.join(["%02x" % ord(c) for c in data[i:i+16]]) + ' '
+        s[-1] += (8 + 16*3 - len(s[-1])) * ' '
+        s[-1] += ''.join([c if c in printable else '.' for c in data[i:i+16]])
+    return '\n'.join(s)
 
