8156e8ac927e3d4b235c3969a89010df1b7418b8
stephen.tiedemann@googlemail.com <>
2011-09-27 19:35:02 +0200

    adapted --device option to new reader search that allows specific usb or tty devices be selected

--- examples/snep-test-client.py
+++ examples/snep-test-client.py
@@ -280,7 +280,10 @@ def test_07():
 
 def main():
     general_bytes = nfc.llcp.startup({'send-lto': 1000, 'recv-miu': 1024})
-    clf = nfc.ContactlessFrontend(options.device)
+    for device in options.device:
+        try: clf = nfc.ContactlessFrontend(device); break
+        except LookupError: pass
+    else: return
 
     peer = llcp_connect(clf, general_bytes)
     if peer is None: return
@@ -345,8 +348,11 @@ if __name__ == '__main__':
                       action="store", dest="logfile",
                       help="write log messages to LOGFILE")
     parser.add_option("--device", type="string", default=[],
-                      action="append", dest="device", metavar="NAME",
-                      help="use this device ('ipsim' for TCP/IP simulation)")
+                      action="append", dest="device", metavar="SPEC",
+                      help="use only device(s) according to SPEC: "\
+                          "usb[:vendor[:product]] (vendor and product in hex) "\
+                          "usb[:bus[:dev]] (bus and device number in decimal) "\
+                          "tty[:(usb|com)[:port]] (usb virtual or com port)")
     parser.add_option("--mode", type="choice", default=None,
                       choices=["target", "initiator"],
                       action="store", dest="mode",
@@ -380,4 +386,8 @@ if __name__ == '__main__':
             log.info("enable debug output for module '{0}'".format(module))
             logging.getLogger(module).setLevel(logging.DEBUG)
 
+    if len(options.device) == 0:
+        # search and use first
+        options.device = ["",]
+        
     main()
