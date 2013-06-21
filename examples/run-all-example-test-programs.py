f1a22f74bdc70dc8473ca708a70d2bf229a47a7a
stephen.tiedemann@googlemail.com <>
2013-06-21 16:12:15 +0200

    small script to execute, over udp, all tests for all example test programs with a single command

---
+++ examples/run-all-example-test-programs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2013 Stephen Tiedemann <stephen.tiedemann@gmail.com>
+#
+# Licensed under the EUPL, Version 1.1 or - as soon they 
+# will be approved by the European Commission - subsequent
+# versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+#
+# http://www.osor.eu/eupl
+#
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+# -----------------------------------------------------------------------------
+
+from subprocess import Popen, PIPE, STDOUT
+import shlex
+import sys
+import os
+
+test_programs = [
+    ("llcp-test-server.py --mode t", "llcp-test-client.py --mode i"),
+    ("llcp-test-server.py --mode i", "llcp-test-client.py --mode t"),
+    ("snep-test-server.py --mode t", "snep-test-client.py --mode i"),
+    ("snep-test-server.py --mode i", "snep-test-client.py --mode t"),
+    ("handover-test-server.py --mode t", "handover-test-client.py --mode i"),
+    ("handover-test-server.py --mode i", "handover-test-client.py --mode t"),
+    ("phdc-test-manager.py --mode i", "phdc-test-agent.py p2p"),
+    ("phdc-test-manager.py --mode i", "phdc-test-agent.py tag"),
+    ]
+
+for server, client in test_programs:
+    print "*** {0} ***".format(client)
+    
+    server = sys.path[0] + "/{0} --device udp -q".format(server)
+    server = Popen(shlex.split(server), stderr=PIPE, stdout=PIPE)
+
+    client = sys.path[0] + "/{0} --device udp -q -T".format(client)
+    client = Popen(shlex.split(client), stderr=STDOUT)
+
+    client.wait()
+    server.wait()
