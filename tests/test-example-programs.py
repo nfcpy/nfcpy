7e6bab5469b9455b872b99eacbc01a7a11a8f386
stephen.tiedemann@googlemail.com <>
2013-06-25 17:13:25 +0200

    also test with largest miu; allow to set driver on invocation; print time elapsed after completion

--- tests/test-example-programs.py
+++ tests/test-example-programs.py
@@ -23,33 +23,53 @@
 
 from subprocess import Popen, PIPE, STDOUT
 import shlex
+import time
 import sys
 import os
 
 test_programs = [
-    ("llcp-test-server.py --mode t", "llcp-test-client.py --mode i"),
-    ("llcp-test-server.py --mode i", "llcp-test-client.py --mode t"),
-    ("snep-test-server.py --mode t", "snep-test-client.py --mode i"),
-    ("snep-test-server.py --mode i", "snep-test-client.py --mode t"),
-    ("handover-test-server.py --mode t", "handover-test-client.py --mode i"),
-    ("handover-test-server.py --mode i", "handover-test-client.py --mode t"),
-    ("phdc-test-manager.py --mode t", "phdc-test-agent.py p2p --mode i"),
-    ("phdc-test-manager.py --mode i", "phdc-test-agent.py p2p --mode t"),
-    ("phdc-test-manager.py --mode i --loop", "phdc-test-agent.py tag"),
+    ("llcp-test-server.py --mode t --miu 128",
+     "llcp-test-client.py --mode i --miu 128"),
+    ("llcp-test-server.py --mode i --miu 128",
+     "llcp-test-client.py --mode t --miu 128"),
+    ("llcp-test-server.py --mode t --miu 2176",
+     "llcp-test-client.py --mode i --miu 2176"),
+    ("llcp-test-server.py --mode i --miu 2176",
+     "llcp-test-client.py --mode t --miu 2176"),
+    ("snep-test-server.py --mode t",
+     "snep-test-client.py --mode i"),
+    ("snep-test-server.py --mode i",
+     "snep-test-client.py --mode t"),
+    ("handover-test-server.py --mode t",
+     "handover-test-client.py --mode i"),
+    ("handover-test-server.py --mode i",
+     "handover-test-client.py --mode t"),
+    ("phdc-test-manager.py --mode t",
+     "phdc-test-agent.py p2p --mode i"),
+    ("phdc-test-manager.py --mode i",
+     "phdc-test-agent.py p2p --mode t"),
+    ("phdc-test-manager.py --mode i --loop",
+     "phdc-test-agent.py tag"),
     ]
 
 examples = os.path.split(sys.path[0])[0] + "/examples/"
+device = sys.argv[1] if len(sys.argv) > 1 else 'udp'
 
+started = time.time()
 for server, client in test_programs:
     print "*** {0} ***".format(client)
     
-    server = examples + "{0} --device udp -q".format(server)
+    server = examples + "{0} --device {1} -q".format(server, device)
     print server
     server = Popen(shlex.split(server), stderr=PIPE, stdout=PIPE)
 
-    client = examples + "{0} --device udp -q -T".format(client)
+    client = examples + "{0} --device {1} -q -T".format(client, device)
     print client
     client = Popen(shlex.split(client), stderr=STDOUT)
 
     client.wait()
     server.terminate()
+
+elapsed = int(time.time() - started)
+print("completed tests in {0} minutes {1} seconds"
+      .format(elapsed/60, elapsed%60))
