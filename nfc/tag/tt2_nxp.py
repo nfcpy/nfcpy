aeca662126423951261fcbc716fea72377ea24fb
stephen.tiedemann@gmail.com <>
2014-11-14 18:06:35 +0100

    renamed parameter in protect() to be more generic across tag types

--- nfc/tag/tt2_nxp.py
+++ nfc/tag/tt2_nxp.py
@@ -103,7 +103,7 @@ class MifareUltralightC(tt2.Type2Tag):
 
         return s
 
-    def protect(self, password=None, read_protect=False, from_page=0):
+    def protect(self, password=None, read_protect=False, protect_from=0):
         if password is not None:
             if password == "":
                 # set the factory key
@@ -120,7 +120,7 @@ class MifareUltralightC(tt2.Type2Tag):
             self.write(46, key2[0:4])
             self.write(47, key2[4:8])
             # protect from memory page
-            self[42*4] = from_page
+            self[42*4] = protect_from
             # set or unset read protection
             self[43*4] = 0x00 if read_protect else 0x01
             self.synchronize()
@@ -234,7 +234,7 @@ class NTAG21x(tt2.Type2Tag):
         except nfc.clf.TimeoutError:
             return False
 
-    def protect(self, password=None, read_protect=False, from_page=0):
+    def protect(self, password=None, read_protect=False, protect_from=0):
         log.debug("protect tag")
         if password is not None:
             if password == "":
@@ -251,7 +251,7 @@ class NTAG21x(tt2.Type2Tag):
             for i in range(6):
                 self[cfgaddr+8+i] = key[i]
             # start protection from page
-            self[cfgaddr+3] = from_page
+            self[cfgaddr+3] = protect_from
             # set/clear protection bit
             self[cfgaddr+4] = (self[cfgaddr+4] & 0x7F) | (read_protect << 7)
             self.synchronize()
