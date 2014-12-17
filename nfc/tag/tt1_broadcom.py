808957a9fb03d1c609236d8af89faa0be1420b37
stephen.tiedemann@gmail.com <>
2014-12-17 12:08:58 +0100

    updated product names; moved activate to end of file

--- nfc/tag/tt1_broadcom.py
+++ nfc/tag/tt1_broadcom.py
@@ -27,34 +27,35 @@ import os
 
 from . import tt1
 
-def activate(clf, target):
-    hrom = clf.exchange("\x78\x00\x00\x00\x00\x00\x00", timeout=0.01)[0:2]
-    if hrom == "\x11\x48":
-        return Topaz96(clf, target)
-    if hrom == "\x12\x4C":
-        return Topaz512(clf, target)
-    return None
-
-class Topaz96(tt1.Type1Tag):
+class Topaz(tt1.Type1Tag):
     def __init__(self, clf, target):
-        super(Topaz96, self).__init__(clf, target)
-        self._product = "Topaz-96 (IRT-5011)"
+        super(Topaz, self).__init__(clf, target)
+        self._product = "Topaz (BCM20203T96)"
 
     def dump(self):
-        return super(Topaz96, self)._dump(stop=15)
+        return super(Topaz, self)._dump(stop=15)
 
     def _format(self, version, wipe):
         tag_memory = tt1.Type1TagMemoryReader(self)
         tag_memory[8:14] = "\xE1\x10\x0E\x00\x03\x00"
+        
+        if version is not None:
+            if version >> 4 == 1:
+                tag_memory[9] = version
+            else:
+                log.warning("can not format with major version != 1")
+                return False
+        
         if wipe is not None:
             tag_memory[14:104] = 90 * chr(wipe & 0xFF)
+        
         tag_memory.synchronize()
         return True
 
 class Topaz512(tt1.Type1Tag):
     def __init__(self, clf, target):
         super(Topaz512, self).__init__(clf, target)
-        self._product = "Topaz-512 (TPZ-505-016)"
+        self._product = "Topaz 512 (BCM20203T512)"
 
     def dump(self):
         return super(Topaz512, self)._dump(stop=64)
@@ -63,9 +64,26 @@ class Topaz512(tt1.Type1Tag):
         tag_memory = tt1.Type1TagMemoryReader(self)
         tag_memory[ 8:16] = ("E1103F00" "0103F230").decode("hex")
         tag_memory[16:24] = ("330203F0" "02030300").decode("hex")
+        
+        if version is not None:
+            if version >> 4 == 1:
+                tag_memory[9] = version
+            else:
+                log.warning("can not format with major version != 1")
+                return False
+        
         if wipe is not None:
             tag_memory[ 24:104] =  80 * chr(wipe & 0xFF)
             tag_memory[128:512] = 384 * chr(wipe & 0xFF)
+        
         tag_memory.synchronize()
         return True
+
+def activate(clf, target):
+    hrom = clf.exchange("\x78\x00\x00\x00\x00\x00\x00", timeout=0.01)[0:2]
+    if hrom == "\x11\x48":
+        return Topaz(clf, target)
+    if hrom == "\x12\x4C":
+        return Topaz512(clf, target)
+    return None
 
