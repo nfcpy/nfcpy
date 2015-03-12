45fa8bbebed57a77daa5a685afdd64b1910095b6
stephen.tiedemann@gmail.com <>
2015-03-12 12:36:29 +0100

    added technology choice flag and log message colors

--- examples/cli.py
+++ examples/cli.py
@@ -51,7 +51,7 @@ class CommandLineInterface(object):
         
         self.options = argument_parser.parse_args()
 
-        ch = logging.StreamHandler()
+        ch = ColorStreamHandler() #logging.StreamHandler()
         lv = logging.ERROR if self.options.quiet else logging.DEBUG
         lv = logging.INFO if self.options.logfile else lv
         ch.setLevel(lv)
@@ -120,6 +120,9 @@ class CommandLineInterface(object):
         group.add_argument(
             "--wait", action="store_true",
             help="wait until tag removed (implicit with '-l')")
+        group.add_argument(
+            "--technology", choices=list("ABFabf"), metavar="{A,B,F}",
+            help="poll for a single technology (default: all)")
 
     def add_card_options(self, argument_parser):
         group = argument_parser.add_argument_group(
@@ -239,6 +242,12 @@ class CommandLineInterface(object):
                 'on-startup': self.on_rdwr_startup,
                 'on-connect': self.on_rdwr_connect,
                 }
+            if self.options.technology:
+                rdwr_options["targets"] = {
+                    "A": [nfc.clf.TTA(br=106)],
+                    "B": [nfc.clf.TTB(br=106)],
+                    "F": [nfc.clf.TTF(br=424), nfc.clf.TTF(br=212)],
+                }[self.options.technology.upper()]
         else:
             rdwr_options = None
         
@@ -282,3 +291,124 @@ class CommandLineInterface(object):
     def run(self):
         while self.run_once() and self.options.loop:
             log.info("*** RESTART ***")
+
+
+# ColorStreamHandler for python logging framework.
+# based on: http://stackoverflow.com/questions/384076/1336640#1336640
+ 
+# Copyright (c) 2014 Markus Pointner
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import logging
+
+class _AnsiColorStreamHandler(logging.StreamHandler):
+    DEFAULT = '\x1b[0m'
+    RED     = '\x1b[31m'
+    GREEN   = '\x1b[32m'
+    YELLOW  = '\x1b[33m'
+    CYAN    = '\x1b[36m'
+
+    CRITICAL = RED
+    ERROR    = RED
+    WARNING  = YELLOW
+    INFO     = GREEN
+    DEBUG    = CYAN
+
+    @classmethod
+    def _get_color(cls, level):
+        if level >= logging.CRITICAL:  return cls.CRITICAL
+        elif level >= logging.ERROR:   return cls.ERROR
+        elif level >= logging.WARNING: return cls.WARNING
+        elif level >= logging.INFO:    return cls.INFO
+        elif level >= logging.DEBUG:   return cls.DEBUG
+        else:                          return cls.DEFAULT
+
+    def __init__(self, stream=None):
+        logging.StreamHandler.__init__(self, stream)
+
+    def format(self, record):
+        text = logging.StreamHandler.format(self, record)
+        color = self._get_color(record.levelno)
+        return color + text + self.DEFAULT
+
+class _WinColorStreamHandler(logging.StreamHandler):
+    # wincon.h
+    FOREGROUND_BLACK     = 0x0000
+    FOREGROUND_BLUE      = 0x0001
+    FOREGROUND_GREEN     = 0x0002
+    FOREGROUND_CYAN      = 0x0003
+    FOREGROUND_RED       = 0x0004
+    FOREGROUND_MAGENTA   = 0x0005
+    FOREGROUND_YELLOW    = 0x0006
+    FOREGROUND_GREY      = 0x0007
+    FOREGROUND_INTENSITY = 0x0008 # foreground color is intensified.
+    FOREGROUND_WHITE     = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED
+
+    BACKGROUND_BLACK     = 0x0000
+    BACKGROUND_BLUE      = 0x0010
+    BACKGROUND_GREEN     = 0x0020
+    BACKGROUND_CYAN      = 0x0030
+    BACKGROUND_RED       = 0x0040
+    BACKGROUND_MAGENTA   = 0x0050
+    BACKGROUND_YELLOW    = 0x0060
+    BACKGROUND_GREY      = 0x0070
+    BACKGROUND_INTENSITY = 0x0080 # background color is intensified.
+
+    DEFAULT  = FOREGROUND_WHITE
+    CRITICAL = BACKGROUND_YELLOW | FOREGROUND_RED | FOREGROUND_INTENSITY \
+               | BACKGROUND_INTENSITY
+    ERROR    = FOREGROUND_RED | FOREGROUND_INTENSITY
+    WARNING  = FOREGROUND_YELLOW | FOREGROUND_INTENSITY
+    INFO     = FOREGROUND_GREEN
+    DEBUG    = FOREGROUND_CYAN
+
+    @classmethod
+    def _get_color(cls, level):
+        if level >= logging.CRITICAL:  return cls.CRITICAL
+        elif level >= logging.ERROR:   return cls.ERROR
+        elif level >= logging.WARNING: return cls.WARNING
+        elif level >= logging.INFO:    return cls.INFO
+        elif level >= logging.DEBUG:   return cls.DEBUG
+        else:                          return cls.DEFAULT
+
+    def _set_color(self, code):
+        import ctypes
+        ctypes.windll.kernel32.SetConsoleTextAttribute(self._outhdl, code)
+
+    def __init__(self, stream=None):
+        logging.StreamHandler.__init__(self, stream)
+        # get file handle for the stream
+        import ctypes, ctypes.util
+        crtname = ctypes.util.find_msvcrt()
+        crtlib = ctypes.cdll.LoadLibrary(crtname)
+        self._outhdl = crtlib._get_osfhandle(stream.fileno())
+
+    def emit(self, record):
+        color = self._get_color(record.levelno)
+        self._set_color(color)
+        logging.StreamHandler.emit(self, record)
+        self._set_color(self.FOREGROUND_WHITE)
+
+# select ColorStreamHandler based on platform
+import platform
+if platform.system() == 'Windows':
+    ColorStreamHandler = _WinColorStreamHandler
+else:
+    ColorStreamHandler = _AnsiColorStreamHandler
