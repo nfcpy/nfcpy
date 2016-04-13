22b09e571c494485ee348c4ab03f49df57b5f147
stephen.tiedemann@gmail.com <>
2016-04-13 10:30:48 +0200

    submit only the nfc library part to pypi and use the version from nfc.__version__ string

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+"""Setup module for nfcpy.
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
+from setuptools import setup
 from codecs import open
 from os import path
+import nfc
 
 here = path.abspath(path.dirname(__file__))
 
@@ -18,27 +13,19 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='nfcpy',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.11.0',
-
-    description='Python module for Near Field Communication',
-    long_description=long_description,
-
-    # Project homepage
+    name = 'nfcpy',
+    version = nfc.__version__,
+    packages = ['nfc'],
+    license = 'EUPL',
     url = 'https://launchpad.net/nfcpy',
 
-    # Author details
+    description = 'Python module for Near Field Communication',
+    long_description = long_description,
+    keywords = 'contactless nfc llcp p2p ndef',
+
     author = 'Stephen Tiedemann',
     author_email = 'stephen.tiedemann@gmail.com',
 
-    # License
-    license = 'EUPL',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: European Union Public Licence 1.1 (EUPL 1.1)',
@@ -50,12 +37,5 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
-    # Project keywords
-    keywords = 'contactless nfc llcp p2p ndef',
-
-    # List of packages
-    packages = find_packages(exclude=['docs', 'tests', 'tools']),
-
-    # Run-time dependencies
     install_requires = ['libusb1', 'pyserial', 'docopt'],
 )
