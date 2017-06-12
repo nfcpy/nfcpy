1c08f73cb85783956ef021951872bde4dc03a687
Stefan Hassferter <stefan.hassferter@crossbario.com>
2017-06-12 13:11:08 +0200

    fix

--- src/nfc/clf/i2c.py
+++ src/nfc/clf/i2c.py
@@ -1,1 +1,2 @@
-""" This file contains the i2c communcation class for communication between RPI and M24SR64"""
+""" This file contains the i2c communcation class for
+    communication between RPI and M24SR64"""
