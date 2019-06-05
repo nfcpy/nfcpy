cf3cca8e6bba456621be08d097d8a7f0e546ca5e
Stephen Tiedemann <stephen.tiedemann@gmail.com>
2019-06-06 00:40:01 +0200

    Switch to use ndeflib.

--- examples/beam.py
+++ examples/beam.py
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 # Copyright 2013 Stephen Tiedemann <stephen.tiedemann@gmail.com>
 #
-# Licensed under the EUPL, Version 1.1 or - as soon they 
+# Licensed under the EUPL, Version 1.1 or - as soon they
 # will be approved by the European Commission - subsequent
 # versions of the EUPL (the "Licence");
 # You may not use this work except in compliance with the
@@ -20,73 +20,68 @@
 # See the Licence for the specific language governing
 # permissions and limitations under the Licence.
 # -----------------------------------------------------------------------------
-
-import logging
-
-log = logging.getLogger('main')
-
-import io
-import time
-import random
-import argparse
 import threading
 import mimetypes
-import sys
-
-from cli import CommandLineInterface
-
+import argparse
+import logging
+import random
+import time
+import ndef
 import nfc
-import nfc.snep
-import nfc.ndef
+import cli
+import sys
+import io
 
-filetype = file if sys.version_info[0] == 2 else io.IOBase
+
+log = logging.getLogger('main')
+filetype = file if sys.version_info.major < 3 else io.IOBase  # noqa: F821
 
 
 def add_send_parser(parser):
     subparsers = parser.add_subparsers(
-            title="send item", dest="send_action", description="""\
-            Construct beam data from the send item and transmit to the peer 
-            device when touched. Use 'beam.py send {item} -h' to learn 
-            additional and/or required arguments per send item.""")
+        title="send item", dest="send_action", description="""
+        Construct beam data from the send item and transmit to the peer
+        device when touched. Use 'beam.py send {item} -h' to learn
+        additional and/or required arguments per send item.""")
     subparsers.required = True
     add_send_link_parser(subparsers.add_parser(
-            "link", help="send hyperlink", description="""\
-            Construct an NDEF Smartposter message with URI and the optional
-            title argument and send it to the peer when connected."""))
+        "link", help="send hyperlink", description="""
+        Construct an NDEF Smartposter message with URI and the optional
+        title argument and send it to the peer when connected."""))
     add_send_text_parser(subparsers.add_parser(
-            "text", help="send plain text", description="""\
-            Construct an NDEF Text message with the input string as 
-            text content. The language default is 'en' (English) but may be set
-            differently with the --lang option."""))
+        "text", help="send plain text", description="""
+        Construct an NDEF Text message with the input string as
+        text content. The language default is 'en' (English) but may be set
+        differently with the --lang option."""))
     add_send_file_parser(subparsers.add_parser(
-            "file", help="send disk file", description="""\
-            Embed the file content into an NDEF message and send 
-            it to the peer when connected. The message type is guessed from 
-            the file type using the mimetype module and the message name is 
-            set to the file name unless explicitly set with command line flags 
-            -t TYPE and -n NAME, respectively."""))
+        "file", help="send disk file", description="""
+        Embed the file content into an NDEF message and send
+        it to the peer when connected. The message type is guessed from
+        the file type using the mimetype module and the message name is
+        set to the file name unless explicitly set with command line flags
+        -t TYPE and -n NAME, respectively."""))
     add_send_ndef_parser(subparsers.add_parser(
-            "ndef", help="send ndef data", description="""\
-            Send an NDEF message from FILE. If the file contains 
-            multiple messages the strategy that determines the message to be 
-            send can be set with the --select argument. For strategies that 
-            select a different message per touch beam.py must be called with 
-            the --loop flag. The strategies 'first', 'last' and 'random' 
-            select the first, last or a random message from the file. The 
-            strategies 'next' and 'cycle' start with the first message and 
-            then count up, the difference is that 'next' stops at the last 
-            message while 'cycle' continues with first."""))
+        "ndef", help="send ndef data", description="""
+        Send an NDEF message from FILE. If the file contains
+        multiple messages the strategy that determines the message to be
+        send can be set with the --select argument. For strategies that
+        select a different message per touch beam.py must be called with
+        the --loop flag. The strategies 'first', 'last' and 'random'
+        select the first, last or a random message from the file. The
+        strategies 'next' and 'cycle' start with the first message and
+        then count up, the difference is that 'next' stops at the last
+        message while 'cycle' continues with first."""))
     parser.add_argument(
-            "--timeit", action="store_true", help="measure transfer time")
+        "--timeit", action="store_true", help="measure transfer time")
 
 
 def send_message(args, llc, message):
     t0 = time.time() if args.timeit else None
-    if not nfc.snep.SnepClient(llc).put(message):
+    if not nfc.snep.SnepClient(llc).put_records(message):
         log.error("failed to send message")
     if t0 is not None:
         transfer_time = time.time() - t0
-        message_size = len(str(message))
+        message_size = len(b''.join(ndef.message_encoder(message)))
         print("message sent in {0:.3f} seconds ({1} byte @ {2:.0f} byte/sec)"
               .format(transfer_time, message_size,
                       message_size / transfer_time))
@@ -101,10 +96,10 @@ def add_send_link_parser(parser):
 
 
 def run_send_link_action(args, llc):
-    sp = nfc.ndef.SmartPosterRecord(args.uri)
+    record = ndef.SmartposterRecord(args.uri)
     if args.title:
-        sp.title = args.title
-    send_message(args, llc, nfc.ndef.Message(sp))
+        record.set_title(args.title)
+    send_message(args, llc, [record])
 
 
 def add_send_text_parser(parser):
@@ -116,10 +111,10 @@ def add_send_text_parser(parser):
 
 
 def run_send_text_action(args, llc):
-    record = nfc.ndef.TextRecord(args.text)
+    record = ndef.TextRecord(args.text)
     if args.lang:
         record.language = args.lang
-    send_message(args, llc, nfc.ndef.Message(record))
+    send_message(args, llc, [record])
 
 
 def add_send_file_parser(parser):
@@ -143,14 +138,9 @@ def run_send_file_action(args, llc):
     if args.name is None:
         args.name = args.file.name if args.file.name != "<stdin>" else ""
 
-    data = args.file.read()
-    try:
-        data = data.decode("hex")
-    except TypeError:
-        pass
-
-    record = nfc.ndef.Record(args.type, args.name, data)
-    send_message(args, llc, nfc.ndef.Message(record))
+    record = ndef.Record(args.type, args.name, args.file.read())
+    log.info("send {}".format(record))
+    send_message(args, llc, [record])
 
 
 def add_send_ndef_parser(parser):
@@ -167,14 +157,13 @@ def add_send_ndef_parser(parser):
 
 def run_send_ndef_action(args, llc):
     if isinstance(args.ndef, filetype):
-        bytestream = io.BytesIO(args.ndef.read())
-        args.ndef = list()
-        while True:
-            try:
-                args.ndef.append(nfc.ndef.Message(bytestream))
-            except nfc.ndef.LengthError:
-                break
+        octets = io.BytesIO(args.ndef.read())
         args.selected = -1
+        args.ndef = list()
+        records = list(ndef.message_decoder(octets, known_types={}))
+        while records:
+            args.ndef.append(records)
+            records = list(ndef.message_decoder(octets, known_types={}))
 
     if args.select == "first":
         args.selected = 0
@@ -187,64 +176,65 @@ def run_send_ndef_action(args, llc):
     elif args.select == "random":
         args.selected = random.choice(range(len(args.ndef)))
 
-    if args.selected < len(args.ndef):
+    if 0 <= args.selected < len(args.ndef):
+        log.info("send {}".format(args.ndef[args.selected]))
         send_message(args, llc, args.ndef[args.selected])
 
 
 def add_recv_parser(parser):
     subparsers = parser.add_subparsers(
-            title="receive action", dest="recv_action", description="""\
-            On receipt of incoming beam data perform the specified
-            action. Use 'beam.py recv {action} -h' to learn additional and/or
-            required arguments per action.""")
+        title="receive action", dest="recv_action", description="""
+        On receipt of incoming beam data perform the specified
+        action. Use 'beam.py recv {action} -h' to learn additional and/or
+        required arguments per action.""")
     subparsers.required = True
     add_recv_print_parser(subparsers.add_parser(
-            "print", help="print received message", description="""\
-            Print the received NDEF message and do nothing else."""))
+        "print", help="print received message", description="""
+        Print the received NDEF message and do nothing else."""))
     add_recv_save_parser(subparsers.add_parser(
-            "save", help="save ndef data to a disk file", description="""\
-            Save incoming beam data to a file. New data is appended
-            if the file does already exist, a parser can use the NDEF message
-            begin and end flags to separate messages."""))
+        "save", help="save ndef data to a disk file", description="""
+        Save incoming beam data to a file. New data is appended
+        if the file does already exist, a parser can use the NDEF message
+        begin and end flags to separate messages."""))
     add_recv_echo_parser(subparsers.add_parser(
-            "echo", help="send ndef data back to peer device", description="""\
-            Receive an NDEF message and send it back to the peer device 
-            without any modification."""))
+        "echo", help="send ndef data back to peer device", description="""
+        Receive an NDEF message and send it back to the peer device
+        without any modification."""))
     add_recv_send_parser(subparsers.add_parser(
-            "send", help="receive data and send an answer", description="""\
-            Receive an NDEF message and use the translations file 
-            to find a matching response to send to the peer device. Each 
-            translation is a pair of in and out NDEF message cat together."""))
+        "send", help="receive data and send an answer", description="""
+        Receive an NDEF message and use the translations file
+        to find a matching response to send to the peer device. Each
+        translation is a pair of in and out NDEF message cat together."""))
 
 
 def add_recv_print_parser(parser):
     parser.set_defaults(func=run_recv_print_action)
 
 
-def run_recv_print_action(args, llc, rcvd_ndef_msg):
-    log.info('print ndef message {0!r}'.format(rcvd_ndef_msg.type))
-    print(rcvd_ndef_msg.pretty())
+def run_recv_print_action(args, llc, message):
+    for record in message:
+        print(record)
 
 
 def add_recv_save_parser(parser):
     parser.set_defaults(func=run_recv_save_action)
     parser.add_argument(
             "file", type=argparse.FileType('a+b'),
-            help="write ndef to file ('-' write to stdout)")
+            help="write ndef data octets to file")
 
 
-def run_recv_save_action(args, llc, rcvd_ndef_msg):
-    log.info('save ndef message {0!r}'.format(rcvd_ndef_msg.type))
-    args.file.write(str(rcvd_ndef_msg))
+def run_recv_save_action(args, llc, message):
+    log.info('save ndef message {}'.format(message[0].type))
+    args.file.write(b''.join(ndef.message_encoder(message)))
 
 
 def add_recv_echo_parser(parser):
     parser.set_defaults(func=run_recv_echo_action)
 
 
-def run_recv_echo_action(args, llc, rcvd_ndef_msg):
-    log.info('echo ndef message {0!r}'.format(rcvd_ndef_msg.type))
-    nfc.snep.SnepClient(llc).put(rcvd_ndef_msg)
+def run_recv_echo_action(args, llc, message):
+    log.info('echo ndef message {}'.format(message[0].type))
+    nfc.snep.SnepClient(llc).put_records(message)
 
 
 def add_recv_send_parser(parser):
@@ -254,25 +244,27 @@ def add_recv_send_parser(parser):
             help="echo translations file")
 
 
-def run_recv_send_action(args, llc, rcvd_ndef_msg):
-    log.info('translate ndef message {0!r}'.format(rcvd_ndef_msg.type))
+def run_recv_send_action(args, llc, message):
+    log.info('translate ndef message {}'.format(message[0].type))
+
     if isinstance(args.translations, filetype):
-        bytestream = io.BytesIO(args.translations.read())
+        stream = io.BytesIO(args.translations.read())
         args.translations = list()
         while True:
             try:
-                msg_recv = nfc.ndef.Message(bytestream)
-                msg_send = nfc.ndef.Message(bytestream)
+                msg_recv = list(ndef.message_decoder(stream, known_records={}))
+                msg_send = list(ndef.message_decoder(stream, known_records={}))
                 args.translations.append((msg_recv, msg_send))
-                log.info('added translation {0!r} => {1:!r}'.format(
+                log.info('added translation {} => {}'.format(
                         msg_recv, msg_send))
-            except nfc.ndef.LengthError:
+            except ndef.DecodeError:
                 break
+
     for msg_recv, msg_send in args.translations:
-        if msg_recv == rcvd_ndef_msg:
-            log.info('rcvd beam {0!r}'.format(msg_recv))
-            log.info('send beam {0!r}'.format(msg_send))
-            nfc.snep.SnepClient(llc).put(msg_send)
+        if message == msg_recv:
+            log.info('rcvd beam {}'.format(msg_recv))
+            log.info('send beam {}'.format(msg_send))
+            nfc.snep.SnepClient(llc).put_records(msg_send)
             break
 
 
@@ -281,14 +273,14 @@ class DefaultServer(nfc.snep.SnepServer):
         self.args, self.llc = args, llc
         super(DefaultServer, self).__init__(llc)
 
-    def put(self, ndef_message):
+    def process_put_request(self, ndef_message):
         log.info("default snep server got put request")
         if self.args.action == "recv":
             self.args.func(self.args, self.llc, ndef_message)
         return nfc.snep.Success
 
 
-class Main(CommandLineInterface):
+class Main(cli.CommandLineInterface):
     def __init__(self):
         parser = argparse.ArgumentParser()
         subparsers = parser.add_subparsers(
